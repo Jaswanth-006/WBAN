d86e6f93b086a538f9d8a590128d8e939a7ba750
Jaswanth-006 <jaswanth.s006@gmail.com>
2026-02-23 01:36:52 +0530

    Update backend/src/app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AppGateway } from './app.gateway';
 
 @Module({
   imports: [],
-  controllers: [AppController], 
-  providers: [], // IMPORTANT: This must be empty (No AppGateway)
+  controllers: [AppController],
+  providers: [AppGateway], // IMPORTANT: We must register the Gateway here!
 })
 export class AppModule {}
